b8594ac90c939f67368bd71f3eb81c80866ec37a
rewalltech <rewalltech@gmail.com>
2026-08-15 11:38:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,17 @@ import Vinheta from './components/Vinheta';
 
 export default function App() {
   return (
-    <Vinheta />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pedido" element={<Formulario />} />
-        <Route path="/galeria" element={<Galeria />} />
-        <Route path="/patrocinio" element={<Patrocinio />} />
-        <Route path="/historia" element={<Historia />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Vinheta />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pedido" element={<Formulario />} />
+          <Route path="/galeria" element={<Galeria />} />
+          <Route path="/patrocinio" element={<Patrocinio />} />
+          <Route path="/historia" element={<Historia />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
